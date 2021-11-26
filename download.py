662608db2f6bb37af04c1d2a18c61eaae7acfb44
MozillaKab <7033740+mozillakab@users.noreply.github.com>
2021-11-26 14:10:03 +0100

    Update download.py

--- download.py
+++ download.py
@@ -16,31 +16,31 @@ if not os.path.exists('data_traning'):
  os.makedirs('data_traning')
 
 
-
+#16 langues
 tutlayin=['kab','eng','fra','ita','eus','cat','por','spa','deu','nld','swe','est','srp','tur','hun','rus']
-##
-###Téléchargement des corpus à partir de tatoeba
-##for i in tutlayin:
-##    wget.download('https://downloads.tatoeba.org/exports/per_language/'+str(i)+'/'+str(i)+'_sentences.tsv.bz2','data_downloaded')
-##
-##
-###ce programme permet de  dézipper les fichiers de langues
-##
-##
-##for tutlayt in tutlayin:
-##    filename= tutlayt+"_sentences.tsv.bz2"
-##    dirpath='data_downloaded'
-##
-##    archive_path = os.path.join(dirpath,filename)
-##    outfile_path = os.path.join(dirpath, filename[:-4])
-##    with open(archive_path, 'rb') as source, open(outfile_path, 'wb') as dest:
-##        dest.write(bz2.decompress(source.read()))
-##
-###Suppression des fichiers zipés
-##
-##for tutlayt in tutlayin:
-##    afaylu_arewway= 'data_downloaded/'+tutlayt+"_sentences.tsv.bz2"
-##    os.remove(afaylu_arewway)
+
+#Téléchargement des corpus à partir de tatoeba
+for i in tutlayin:
+    wget.download('https://downloads.tatoeba.org/exports/per_language/'+str(i)+'/'+str(i)+'_sentences.tsv.bz2','data_downloaded')
+
+
+#ce programme permet de  dézipper les fichiers de langues
+
+
+for tutlayt in tutlayin:
+    filename= tutlayt+"_sentences.tsv.bz2"
+    dirpath='data_downloaded'
+
+    archive_path = os.path.join(dirpath,filename)
+    outfile_path = os.path.join(dirpath, filename[:-4])
+    with open(archive_path, 'rb') as source, open(outfile_path, 'wb') as dest:
+        dest.write(bz2.decompress(source.read()))
+
+#Suppression des fichiers zipés
+
+for tutlayt in tutlayin:
+    afaylu_arewway= 'data_downloaded/'+tutlayt+"_sentences.tsv.bz2"
+    os.remove(afaylu_arewway)
 
 
 #ce programme permet d'extraire uniquement les phrases du corpus téléchargé sans les autres informations contenues dans ces fichiers
@@ -59,7 +59,7 @@ for tutlayt in tutlayin:
 
 # Ce programme permet d'extraire 2000 phrase de chaque fichier de langue . Il servira à entrainer l'algorithme et générer le modele de langue
 
-limit=1100
+limit=1000
 i=0
 afaylu_yemmden= open("data_traning/LanguageDetection.csv","w+",encoding='utf-8')
 afaylu_yemmden.write("sentence"+'\t'+"language"+'\r')
@@ -67,7 +67,7 @@ for tutlayt in tutlayin:
     i=0
     afaylu_zeddigen=open("data_raw/"+tutlayt+"_sentences.txt",encoding='utf-8')
     for sentence in afaylu_zeddigen:
-        if len(sentence) > 25:
+        if len(sentence) > 15:
             afaylu_yemmden.write(sentence.replace("\n","")+'\t'+tutlayt+'\r')
             i=i+1
             if limit!=0 and i>=limit:
@@ -87,7 +87,7 @@ for tutlayt in tutlayin:
     i=0
     afaylu_zeddigen=open("data_raw/"+tutlayt+"_sentences.txt",encoding='utf-8')
     for sentence in afaylu_zeddigen:
-        if len(sentence) > 25:
+        if len(sentence) > 15:
             if i>5000:
              afaylu_yemmden.write(sentence.replace("\n","").replace('\r',"")+'\r')
             i=i+1
@@ -97,3 +97,4 @@ for tutlayt in tutlayin:
 
     afaylu_zeddigen.close()
 afaylu_yemmden.close()
+print("finished")
